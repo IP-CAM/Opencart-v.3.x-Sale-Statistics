a18914ccda75a6ee484b5d562be120703d44f365
hasanalibalcioglu <h.alibalcioglu@gmail.com>
2020-11-24 02:49:58 +0300

    initial

---
+++ admin/model/custom/sale_stats.php
@@ -0,0 +1,16 @@
+<?php
+class ModelCustomSaleStats extends Model {
+
+    public function getMonthlyTotal() {
+        $query = $this->db->query("SELECT SUM(`total`) 'total_month' FROM `" . DB_PREFIX ."order` WHERE order_status_id > 1 AND date_added >= '" . date('Y-m-d') ."'");
+
+        return $query->row;
+    }
+
+    public function get30Total() {
+        $query = $this->db->query("SELECT SUM(`total`) 'total_30' FROM `" . DB_PREFIX ."order` WHERE order_status_id > 1 AND date_added >= '" . date('Y-m-d', strtotime('-30 days')) ."'");
+
+        return $query->row;
+    }
+}
+?>
